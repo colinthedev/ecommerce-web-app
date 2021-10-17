7f6e9b2e4d9b9f44067a93643cf35f1363a23b62
Colin Peters <colinthedev@gmail.com>
2021-10-17 14:19:06 -0600

    React router added to App / Index, mimicked HatsPage in App

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,7 +47,7 @@ class Directory extends React.Component {
     render() {
         return (
             <div className="directory-menu">
-                {this.state.sections.map(({title, imageUrl, id, size}) => (
+                {this.state.sections.map(({id, title, imageUrl, size}) => (
                     <MenuItem
                         key={id}
                         title={title}
